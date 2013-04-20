c5e6206a40cac28afdcadd3f4e739520b9629488
Chris Yeninas <phantom@sundergaming.com>
2013-04-19 21:55:25 -0400

    Blackout for CISPA

--- blaccupy.js
+++ blaccupy.js
@@ -1,11 +1,12 @@
 // Rich Jones of Gun.io, public domain. Fork me, please! http://github.com/Miserlou/
+// Forked and modified for CISPA relevance by PhantomGamers
 var now = new Date();
 
-if (now.getDate() == 18 && now.getMonth() == 0 && now.getFullYear() == 2012) {
+if (now.getDate() == 22 && now.getMonth() == 3 && now.getFullYear() == 2013) {
 
     window.onload = function(){
       var oBody = document.getElementsByTagName('body')[0]; 
-      oBody.style.cssText = "background: #000000; text-align: center; color: #111; font: 2.2em Helvetica; vertical-align: middle"; 
-      oBody.innerHTML = '<div style="width: 960px; margin: 0 auto;"><br /><br />This website is..<br /> <br /><div style="color: #222222">BLACKED OUT!</div><br />..in protest of <a href="https://en.wikipedia.org/wiki/Stop_Online_Piracy_Act" target="_blank" style="color: #220022">pending</a> <a href="https://en.wikipedia.org/wiki/PROTECT_IP_Act" target="_blank" style="color: #220022">legislation</a> which threatens the freedoms of websites like this one and the freedoms of the people who use them.<br /><br />Please <a href="http://americancensorship.org/" target="_blank" style="color: #220022">help protect our free speech</a> against the corporate and political interests which seek to take them away!<br /><br />(And don\'t worry, we\'ll be back in business tomorrow!)</div>'; 
+      oBody.style.cssText = "background: #000000; text-align: center; color: #3A3A3A; font: 2.2em Helvetica; vertical-align: middle"; 
+      oBody.innerHTML = '<div style="width:960px;margin:0 auto;">' + document.location.hostname + ' is...<br /><br /><div style="text-decoration:underline;">BLACKED OUT!</div><br />...in protest of <a href="https://en.wikipedia.org/wiki/Cyber_Intelligence_Sharing_and_Protection_Act" style="color:#FF0000">pending legislation</a> which threatens the freedoms of websites like this one and the freedoms of the people who use them.<br /><br />Please <a href="http://www.cispaisback.org/" style="color:#FF0000">help protect our free speech</a> against the corporate and political interests which seek to take them away!<br /><br />(And don\'t worry, we\'ll be back in business tomorrow!)</div>';
     }
 }
